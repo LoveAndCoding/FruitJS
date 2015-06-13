5e7938cebb877c583e258a2c877679de275297b5
Andrew Hushbeck <andrew.hushbeck@imgineme.com>
2015-06-13 13:34:50 -0700

    Mocha + Code Coverage
    Switched to using mocha plus instrumented istanbul for code coverage results.

--- test/run.js
+++ test/run.js
@@ -1,10 +1,9 @@
 var execFile = require('child_process').execFile,
 	assert = require('assert'),
-	vows = require('vows'),
 	fs = require('fs-extra'),
 	path = require('path'),
 	
-	FruitJSFile = path.resolve(__dirname, '../bin/cli'),
+	FruitJS = require('../fruit.js'),
 	OutputFolder = path.resolve(__dirname, 'test-output'),
 	
 	testManifestFiles = [
@@ -21,38 +20,38 @@ var execFile = require('child_process').execFile,
 		// , "Resources/folder.gathering.manifest.json" -- Failing; see issue #30
 	];
 
-var execSuite = vows.describe('FruitJS Clean Execution');
-
-function addTestFile(testFile, output) {
-	var testObj = {};
-	
-	testObj['Running FruitJS against '+testFile] = {
-		topic: function () {
-			if(fs.existsSync(output))
+describe('FruitJS Clean Execution', function () {
+	// For now we are simply going to run each and make sure we compile it properly without error.
+	// We are not doing any output checking at the moment
+	for(var t = 0, l = testManifestFiles.length; t < l; t++) {
+		// addTestFile(testManifestFiles[t], OutputFolder + '-' + t);
+		
+		describe(testManifestFiles[t], (function (testFile, output) {
+			
+			before(function (done) {
+				if(fs.existsSync(output))
+					fs.removeSync(output);
+				
+				var manifestFile = path.resolve(__dirname, testFile),
+					manifest = JSON.parse(fs.readFileSync(manifestFile, {encoding:'utf8'})),
+					
+					fruit = new FruitJS(manifest, manifestFile);
+				
+				return fruit.render(output).then(function () {
+					done();
+				}, done);
+			});
+			
+			it('should have the proper output folder ', function () {
+				assert(fs.existsSync(output));
+			});
+			
+			after(function () {
 				fs.removeSync(output);
-			execFile('node', [FruitJSFile, testFile, '-o', output], {cwd: __dirname, timeout: 5000}, this.callback);
-		},
-		'we should report success' : function (err, stdout) {
-			assert.include(stdout.toString(), 'success');
-		},
-		'we should have the proper output folder': function () {
-			assert(fs.existsSync(output));
-		},
-		'we should have nothing in stderr': function (err, stdout, stderr) {
-			assert.isEmpty(stderr.toString());
-		},
-		teardown: function () {
-			fs.removeSync(output);
-		}
-	};
+			});
+			
+		}).bind(this, testManifestFiles[t], OutputFolder + '-' + t));
+		
+	}
 	
-	execSuite.addBatch(testObj);
-}
-
-// For now we are simply going to run each and make sure we compile it properly without error.
-// We are not doing any output checking at the moment
-for(var t = 0, l = testManifestFiles.length; t < l; t++) {
-	addTestFile(testManifestFiles[t], OutputFolder + '-' + t);
-}
-
-execSuite.export(module);
+});
